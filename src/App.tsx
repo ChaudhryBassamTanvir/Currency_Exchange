c1912bb75bd42c8543c69e5d6c6ebfc380171b75
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-05 09:06:41 -0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import { currencyByRupee } from './constants'
 import Button from './components/Button'
@@ -44,9 +44,58 @@ else{
 
 
   return (
-    <View>
-      <Text>App text</Text>
+
+    <>
+
+      <StatusBar/>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+
+        <View style={styles.rupeeContainer}>
+          <Text style={styles.rupee}>PAK RUPEE</Text>
+
+          <TextInput 
+          maxLength={14}
+          value={inputValue}
+          clearButtonMode='always'
+          onChangeText={setInputValue}
+          keyboardType='number-pad'
+          placeholder='Enter amount in Rupees'
+          />
+        </View>
+
+
+        {result &&(
+
+          <Text style={styles.resultTxt}></Text>
+        )}
+      </View>
+
+<View style={styles.bottomContainer}>
+
+<FlatList  
+numColumns={3}
+data={currencyByRupee}
+keyExtractor={item=>item.name}
+renderItem={({item})=>(
+
+<Pressable  style={[styles.button,targetCurrency===item.name && styles.selected]}  
+onPress={()=> buttonPressed(item)}
+>
+
+<Button {...item}/>
+
+
+</Pressable>
+
+
+)}
+/>
+
+</View>
+
     </View>
+    </>
   )
 }
 
