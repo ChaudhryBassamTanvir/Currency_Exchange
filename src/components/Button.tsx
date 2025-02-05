2464c2dd0b2c94c6b6d85c2972c141af3646e088
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-05 08:07:01 -0800

    updated

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,8 @@ type CurrencyButtonProps= PropsWithChildren<{name:"string",flag:"string"}>
 const Button = (props:CurrencyButtonProps) => {
   return (
     <View style={styles.buttonContainer}>
-      <Text>Button</Text>
+      <Text style={styles.flag}>{props.flag}</Text>
+      <Text style={styles.country}>{props.name}</Text>
     </View>
   )
 }
@@ -20,5 +21,18 @@ const styles = StyleSheet.create({
 
 
 
-    buttonContainer:{}
+    buttonContainer:{
+        alignItems:'center'
+    }
+    ,
+    flag:{
+
+        fontSize:28,
+        color:"FFFFFF",
+        marginBottom:5
+    },
+    country:{
+        fontSize:14,
+        color:"2d3436",
+    }
 })
