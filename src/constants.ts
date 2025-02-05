2926299c39b9c5fc2874b2fc76e7744c2796ee30
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-05 07:56:42 -0800

    updated

---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+export const currencyByRupee: Currency[] = [
+    {
+        name: 'DOLLAR',
+        value: 0.0034, // 1 PKR = 0.0034 USD
+        flag: '🇺🇸', // US flag emoji
+        symbol: '$'
+    },
+    {
+        name: 'Euro',
+        value: 0.0031, // 1 PKR = 0.0031 EUR
+        flag: '🇪🇺', // EU flag emoji
+        symbol: '€'
+    },
+    {
+        name: 'POUND',
+        value: 0.0027, // 1 PKR = 0.0027 GBP
+        flag: '🇬🇧', // UK flag emoji
+        symbol: '£'
+    },
+    {
+        name: 'RUBEL',
+        value: 0.29, // 1 PKR = 0.29 RUB
+        flag: '🇷🇺', // Russian flag emoji
+        symbol: '₽'
+    },
+    {
+        name: 'Australian Dollar',
+        value: 0.0051, // 1 PKR = 0.0051 AUD
+        flag: '🇦🇺', // Australian flag emoji
+        symbol: 'A$'
+    },
+    {
+        name: 'Indian Rupee',
+        value: 0.34, // 1 PKR = 0.34 INR
+        flag: '🇮🇳', // Indian flag emoji
+        symbol: '₹'
+    }
+];
