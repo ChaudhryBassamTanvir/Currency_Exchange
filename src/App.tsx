2b0b5338967ec143523d3b27443fdc7ddeeb756a
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-05 08:29:29 -0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { currencyByRupee } from './constants'
 import Button from './components/Button'
+import Snackbar from 'react-native-snackbar'
 
 const App = () => {
+
+const [inputValue,setInputValue]=useState('')
+const [result,setResult]=useState('')
+const [targetCurrency,settargetCurrency]=useState('')
+
+const buttonPressed=(targetValue:Currency)=>{
+  if(!inputValue){
+
+    return Snackbar.show({
+      text:"Enter Value to convert",
+      backgroundColor:"#EA7773",
+      textColor:'#000000'
+
+    })
+  }
+
+const inputAmount=parseFloat(inputValue)
+if (!isNaN(inputAmount)) {
+  const convertValue= inputAmount* targetValue.value
+  const result=`${targetValue.symbol} ${convertValue.toFixed(2)}`
+  setResult(result)
+  settargetCurrency(targetValue.name)
+}
+else{
+  return Snackbar.show({
+    text:"Enter Value to convert",
+    backgroundColor:"#EA7773",
+    textColor:'#000000'
+
+  })
+
+}
+
+
+}
+
+
+
   return (
     <View>
       <Text>App text</Text>
